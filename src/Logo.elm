42884345cc130f7b8641eebb9e0b6e466c701592
Microtribute <46481195+Microtribute@users.noreply.github.com>
2023-10-05 04:18:37 +0800

    update zenith logo

--- src/Logo.elm
+++ src/Logo.elm
@@ -116,7 +116,7 @@ iwc =
 
 zenith : Svg msg
 zenith =
-    clockLogo ( 133.18, 59.58 ) 50 "/assets/images/zenith.svg"
+    clockLogo ( 409, 152 ) 64 "/assets/images/zenith.svg"
 
 
 vacheronConstantin : Svg msg
